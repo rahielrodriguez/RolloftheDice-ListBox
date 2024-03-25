786d08a4a00fce25d8ff65081015ce6193cc3ede
Rahiel Rodriguez <rahielrodriguez@hotmail.com>
2024-03-24 20:35:35 -0600

    Edited Code added

--- RolloftheDiceListBox.vb
+++ RolloftheDiceListBox.vb
@@ -4,19 +4,19 @@ Option Explicit On
 Public Class RolloftheDice
 
     Sub RollingDice()
-        Console.WriteLine("Roll of the Dice")
-        Console.WriteLine()
         Randomize()
         Dim firstRollValue As Integer
         Dim secondRollValue As Integer
         Dim rollingResult As Integer
-        Dim resultsTable(1, 10) As Integer
-        Dim temp As String
+        Dim resultsTable(0, 10) As Integer
+        'Creates an a header that will contain the static values of the possible results of the two dices after rolling them
+        Dim header() = {"2", "3", "4", "05", "06", "07", "08", "09", "10", "11", "12"}
+        Dim headerRow As String = ""
+        Dim dataRow As String = ""
 
-        For i = 0 To 10
-            resultsTable(0, i) = i + 2
-        Next
-
+        'rolls each dice as a random number once and then adds them together.
+        'The result would go into a select case that will add a 1 every time one of the possible results comes out.
+        'Repeats this proces a 1000 times.
         For i = 1 To 1000
             firstRollValue = CInt((Int(Rnd() * 6)) + 1)
             secondRollValue = CInt((Int(Rnd() * 6)) + 1)
@@ -24,41 +24,29 @@ Public Class RolloftheDice
 
             Select Case rollingResult
                 Case Is = 2
-                    resultsTable(1, 0) += 1
+                    resultsTable(0, 0) += 1
                 Case Is = 3
-                    resultsTable(1, 1) += 1
+                    resultsTable(0, 1) += 1
                 Case Is = 4
-                    resultsTable(1, 2) += 1
+                    resultsTable(0, 2) += 1
                 Case Is = 5
-                    resultsTable(1, 3) += 1
+                    resultsTable(0, 3) += 1
                 Case Is = 6
-                    resultsTable(1, 4) += 1
+                    resultsTable(0, 4) += 1
                 Case Is = 7
-                    resultsTable(1, 5) += 1
+                    resultsTable(0, 5) += 1
                 Case Is = 8
-                    resultsTable(1, 6) += 1
+                    resultsTable(0, 6) += 1
                 Case Is = 9
-                    resultsTable(1, 7) += 1
+                    resultsTable(0, 7) += 1
                 Case Is = 10
-                    resultsTable(1, 8) += 1
+                    resultsTable(0, 8) += 1
                 Case Is = 11
-                    resultsTable(1, 9) += 1
+                    resultsTable(0, 9) += 1
                 Case Is = 12
-                    resultsTable(1, 10) += 1
+                    resultsTable(0, 10) += 1
             End Select
         Next
 
-        For row = 0 To 1
-            For column = 0 To 10
-                temp = CStr(resultsTable(row, column)) & " |"
-                Console.Write(temp.PadLeft(6))
-
-            Next
-            Console.WriteLine()
-            Console.WriteLine("------------------------------------------------------------------")
-            Console.WriteLine(StrDup(66, " "))
-        Next
-
-        Console.Read()
     End Sub
 End Class
